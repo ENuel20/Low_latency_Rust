e45cd4a8228181368530d95340d9a9a7185a70ef
enuel20 <emmaonana18@gmail.com>
2026-02-25 17:22:56 +0100

    implemented condvar

---
+++ condvar/src/lib.rs
@@ -0,0 +1,70 @@
+use std::sync::atomic::{AtomicU32, Ordering::Relaxed};
+use std::sync::atomic::AtomicUsize;
+use std::sync::Condvar;
+use atomic_wait::{wait, wake_all, wake_one};
+//use mutex 
+pub struct CondVar {
+    counter: AtomicU32,
+    num_waiters: AtomicUsize,
+}
+
+impl CondVar {
+    pub const fn new() -> Self {
+        Self {
+            counter: AtomicU32::new(0),
+            num_waiters: AtomicUsize::new(0)
+        }
+    }
+
+    pub fn notify_one(&self) {
+        if self.num_waiters.load(Relaxed) > 0{
+        self.counter.fetch_add(1, Relaxed);
+        wake_one(&self.counter);
+    }}
+
+    pub fn notify_all(&self) {
+       if self.num_waiters.load(Relaxed) > 0 {
+        self.counter.fetch_add(1, Relaxed);
+        wake_all(&self.counter);
+    }}
+
+    pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        self.num_waiters.fetch_add(1, Relaxed);
+        let counter_value = self.counter.load(Relaxed);
+        // Unlock the mutex by dropping the guard,
+        // but remember the mutex so we can lock it again later.
+        let mutex = guard.mutex;
+        drop(guard);
+
+        // Wait, but only if the counter hasn't changed since unlocking.
+        wait(&self.counter, counter_value);
+        self.num_waiters.fetch_sub(1, Relaxed);
+        mutex.lock()
+    }
+}
+
+#[test]
+fn test_condvar() {
+    let mutex = Mutex::new(0);
+    let condvar = Condvar::new();
+    let mut wakeups = 0;
+    thread::scope(|s| {
+        s.spawn(|| {
+            thread::sleep(Duration::from_secs(1));
+            *mutex.lock() = 123;
+            condvar.notify_one();
+        });
+        let mut m = mutex.lock();
+        while *m < 100 {
+            m = condvar.wait(m);
+            wakeups += 1;
+        }
+        assert_eq!(*m, 123);
+    });
+    // Check that the main thread actually did wait (not busy-loop),
+    // while still allowing for a few spurious wake ups.
+    assert!(wakeups < 10);
+}
+//fn main() {
+//  println!("Hello, world!");
+//}
---
+++ condvar/src/main.rs
@@ -0,0 +1,58 @@
+pub struct CondVar {
+    counter: AtomicU32,
+}
+
+impl Condvar {
+    pub const fn new() -> Self {
+        Self {
+            counter: AtomicU32::new(0),
+        }
+    }
+
+    pub fn notify_one(&self) {
+        self.counter.fetch_add(1, Relaxed);
+        wake_one(self.counter);
+    }
+
+    pub fn notify_all(&self) {
+        self.counter.fetch_add(1, Relaxed);
+        wake_all(self.counter);
+    }
+
+    pub fn wait(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        let counter_value = self.counter.load(Relaxed);
+        // Unlock the mutex by dropping the guard,
+        // but remember the mutex so we can lock it again later.
+        let mutex = guard.mutex;
+        drop(guard);
+        // Wait, but only if the counter hasn't changed since unlocking.
+        wait(&self.counter, counter_value);
+        mutex.lock();
+    }
+}
+
+#[test]
+fn test_condvar() {
+    let mutex = Mutex::new(0);
+    let condvar = Condvar::new();
+    let mut wakeups = 0;
+    thread::scope(|s| {
+        s.spawn(|| {
+            thread::sleep(Duration::from_secs(1));
+            *mutex.lock() = 123;
+            condvar.notify_one();
+        });
+        let mut m = mutex.lock();
+        while *m < 100 {
+            m = condvar.wait(m);
+            wakeups += 1;
+        }
+        assert_eq!(*m, 123);
+    });
+    // Check that the main thread actually did wait (not busy-loop),
+    // while still allowing for a few spurious wake ups.
+    assert!(wakeups < 10);
+}
+//fn main() {
+//  println!("Hello, world!");
+//}
